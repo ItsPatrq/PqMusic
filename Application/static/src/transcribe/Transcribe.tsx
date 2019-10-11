cf261bf5de095a189530108623aa7230ac0594cc
ItsMeaga1n <peinpat1@gmail.com>
2019-10-11 04:03:04 +0200

    Setup transcription

--- Application/static/src/transcribe/Transcribe.tsx
+++ Application/static/src/transcribe/Transcribe.tsx
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import { Button } from '@blueprintjs/core';
 import Dropzone, { DropEvent, DropzoneState } from 'react-dropzone'
 import DataService from '../dataService/DataService';
+import strings from '../shared/strings';
 
 export class Transcribe extends Component<{}> {
   handleChange(acceptedFiles: File[], rejectedFiles: File[], event: DropEvent) {
     if (acceptedFiles.length > 0) {
-      DataService.Spectrogram(acceptedFiles[0])
+
       //reader.readAsBinaryString(accepted[0]);
     }
     if (rejectedFiles.length > 0) {
@@ -19,13 +20,16 @@ export class Transcribe extends Component<{}> {
       <div className="PqM-Transcribe">
         <div className="PqM-header">
           Transcribe
-            </div>
+          </div>
+        <div>
+          Onsets and Frames
+        </div>
         <Dropzone
           accept='audio/mp3'
           onDrop={this.handleChange}
           multiple={false}
         >
-          {x}
+          {dropzoneContent}
         </Dropzone>
         <br />
         <Button type="submit" className="bp3-intent-primary" text="Wyslij" />
@@ -37,11 +41,11 @@ export class Transcribe extends Component<{}> {
 
 export default Transcribe;
 
-const x = (props: DropzoneState) => (
+const dropzoneContent = (props: DropzoneState) => (
   <section>
     <div {...props.getRootProps()}>
       <input {...props.getInputProps()} />
-      <p>Drag or click to upload your exported composition</p>
+      <p className="PqM-dropZone">{strings.dropZoneDefaultMessage}</p>
     </div>
   </section>
 )
