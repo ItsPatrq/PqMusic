66744974f3afc2263733e61bdff1c03eaba166f6
Patryk Bieszke <peinpat1@gmail.com>
2020-01-15 02:45:22 +0100

    Removal of drums

--- Application/server/transcription/drumsRemoval.py
+++ Application/server/transcription/drumsRemoval.py
@@ -2,8 +2,11 @@
 
 import sys
 from os import path
-from aubio import onset, source
-from numpy import hstack, zeros
+from aubio import onset, source # pylint: disable=no-name-in-module
+import math
+import numpy as np
+from tqdm import tqdm
+from scipy.fftpack import fft, ifft
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from utils.general import loadNormalizedSoundFIle, create_sine, fft_to_hz, hz_to_fft, hz_to_fourier
@@ -29,18 +32,61 @@ def detect_onsets(filePath, method="default", windowSize=1024):
 		if read < hop_s: break
 	return onsets
 
+def remove_a_from_b():
+	raise NotImplementedError()
+	#by spectra or amplitude? ? ?
+
+def cross_correlation(inputData, onsets, exampleSounds, sampleRate, frameWidth=4096, sizeOfZeroPadding=4096, spacing=4096):
+	zeropad = np.zeros(sizeOfZeroPadding)
+	def corr(a, b):
+		div = np.linalg.norm(a) * np.linalg.norm(b) #cosine of angle between those two vectors
+		fftA = fft(np.concatenate((a, zeropad)))
+		fftRevB = fft(list(reversed(np.concatenate((b, zeropad)))))
+		crossCorrelation = abs(ifft(fftA * fftRevB))
+		return crossCorrelation / div
+
+	possibleEvents = {}
+	for onset in onsets:
+		currSoundsCorrelations = []
+		for sound in exampleSounds:
+			currCorrelations = []
+			for i in range(0, int(math.ceil((len(sound) - frameWidth) / spacing))):
+				if len(inputData) < onset+i*spacing:
+					break
+				maxDataLen = min(len(sound) - i*spacing, frameWidth, len(inputData) - (onset+i*spacing))
+				baseSoundWindow = inputData[(onset+i*spacing):(onset+i*spacing+maxDataLen)]
+				testedSoundWindow = sound[(i*spacing):(i*spacing+maxDataLen)]
+				correlation = corr(baseSoundWindow, testedSoundWindow)
+				currCorrelations.append(max(correlation))
+			avg = sum(currCorrelations) / len(currCorrelations)
+			print(avg)
+			currSoundsCorrelations.append(avg)
+		if round(max(currSoundsCorrelations), 1) >= 0.3:
+			possibleEvents[onset] = np.argmax(currSoundsCorrelations)
+	print(possibleEvents)
 
 
 if __name__ == "__main__":
 
-	frameWidth = 8192
-	spacing = 1024
+	frameWidth = 2048
+	spacing = 2048
 	filePath = path.dirname(path.abspath(__file__))
 	#filePath = path.join(filePath, '../test_sounds/chopin-nocturne.wav')
 	#filePath = '../test_sounds/Sine_sequence.wav'
-	filePath = path.join(filePath, '../test_sounds/areFE.wav')
-	sampleRate, data = loadNormalizedSoundFIle(filePath)
+	trackPath = path.join(filePath, '../test_sounds/example/track.wav')
+	sampleRate, data = loadNormalizedSoundFIle(trackPath)
+	sounds = []
+	paths = [
+		path.join(filePath, '../test_sounds/example/track_kick.wav'),
+		path.join(filePath, '../test_sounds/example/track_snare.wav'),
+		path.join(filePath, '../test_sounds/example/track_hihat1.wav'),
+		path.join(filePath, '../test_sounds/example/track_hiHat2.wav'),
+	]
+
+	for path in paths:
+		_, soundData = loadNormalizedSoundFIle(path)
+		sounds.append(soundData)
 	sampleRate = 44100
 
-	onsets = detect_onsets(filePath)
-	print(onsets)
+	onsets = detect_onsets(trackPath)
+	cross_correlation(data, onsets, sounds, sampleRate, frameWidth, frameWidth, spacing)
